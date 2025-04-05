ce77bea60feac06c19585a3dc7ea633cea00b904
balloonflame7 <balloonflame7india@outlook.com>
2025-04-05 17:33:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from pyrogram import filters
 import os
 
 class Config:
-    API_ID = "27079591"
-    API_HASH = "c81ae4c3dc026ea4bf49842a8ce4a5f9"
+    API_ID = "24771953"
+    API_HASH = "2dd99aa9f140d0eacb46368d1dd0994b"
     TOKEN = "8198847203:AAG_HuB34GjqAjN2YFHLiOrpo3btdYz5epE"
     TOKEN = os.environ.get("TOKEN", None)
     MONGO_URL = "MONGO_URI=mongodb+srv://Jatin:709WNkacQIffPFIr@nobino-worm-gpt-ai.pnhvasg.mongodb.net/?retryWrites=true&w=majority&appName=Nobino-worm-gpt-ai"
