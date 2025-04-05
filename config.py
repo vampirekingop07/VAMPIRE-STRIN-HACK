3c58cb27bd8847297018813d5d6e8eb218c63c41
balloonflame7 <balloonflame7india@outlook.com>
2025-04-05 14:32:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 class Config:
     API_ID = "27079591"
     API_HASH = "c81ae4c3dc026ea4bf49842a8ce4a5f9"
-    #TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
+    TOKEN = "8198847203:AAG_HuB34GjqAjN2YFHLiOrpo3btdYz5epE"
     TOKEN = os.environ.get("TOKEN", None)
     MONGO_URL = "mongodb+srv://vivek:1234567890@cluster0.c48d8ih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     START_PIC = "https://files.catbox.moe/ppvvg0.jpg"
