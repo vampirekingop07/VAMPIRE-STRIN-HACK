52a6d90f6169a59b0975dfbec2a61c328e670a17
balloonflame7 <balloonflame7india@outlook.com>
2025-04-05 17:15:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ class Config:
     API_HASH = "c81ae4c3dc026ea4bf49842a8ce4a5f9"
     TOKEN = "8198847203:AAG_HuB34GjqAjN2YFHLiOrpo3btdYz5epE"
     TOKEN = os.environ.get("TOKEN", None)
-    MONGO_URL = "mongodb+srv://vivek:1234567890@cluster0.c48d8ih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    MONGO_URL = "mongodb+srv://balloonflame7:<db_password>@nobino-worm-gpt-ai.pnhvasg.mongodb.net/?retryWrites=true&w=majority&appName=Nobino-worm-gpt-ai"
     START_PIC = "https://files.catbox.moe/ppvvg0.jpg"
-    SUDOERS = filters.user(["6730956183"])
+    SUDOERS = filters.user([""])
