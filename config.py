4af342279944dba21759139fa187cc4f005b7458
balloonflame7 <balloonflame7india@outlook.com>
2025-04-05 17:28:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ class Config:
     API_HASH = "c81ae4c3dc026ea4bf49842a8ce4a5f9"
     TOKEN = "8198847203:AAG_HuB34GjqAjN2YFHLiOrpo3btdYz5epE"
     TOKEN = os.environ.get("TOKEN", None)
-    MONGO_URL = "mongodb+srv://balloonflame7:<db_password>@nobino-worm-gpt-ai.pnhvasg.mongodb.net/?retryWrites=true&w=majority&appName=Nobino-worm-gpt-ai"
+    MONGO_URL = "MONGO_URI=mongodb+srv://Jatin:709WNkacQIffPFIr@nobino-worm-gpt-ai.pnhvasg.mongodb.net/?retryWrites=true&w=majority&appName=Nobino-worm-gpt-ai"
     START_PIC = "https://files.catbox.moe/ppvvg0.jpg"
     SUDOERS = filters.user([""])
