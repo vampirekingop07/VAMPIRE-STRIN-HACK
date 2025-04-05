05e83c33278997282cf3061a2a2784c103fd8b86
balloonflame7 <balloonflame7india@outlook.com>
2025-04-05 18:30:50 +0530

    Update __init__.py

--- RAUSHAN/__init__.py
+++ RAUSHAN/__init__.py
@@ -39,7 +39,7 @@ async def RAUSHAN():
     header = Table(show_header=True, header_style="bold green")
     header.add_column(LOG_TEXT)
     LOG.print(header)
-    LOG.print(f"[bold cyan]𝐓𝐄𝐀𝐌 𝐏𝐔𝐑𝐕𝐈 𝐁𝐎𝐓𝐒")
+    LOG.print(f"[bold cyan]Nobita Bots")
     LOG.print("[bold yellow]𝐘𝐨𝐮𝐫 𝐁𝐨𝐭 𝐒𝐭𝐚𝐫𝐭.............")
     await app.start()    
     
